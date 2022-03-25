1974c1425ca211d3988a4a2bae42e357cfa380a4
Kalpana <kalpana.iyyappan@coles.com.au>
2022-03-25 01:48:24 +0000

    Adding the correctd network module

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
-module "vnet" {
-  source  = "app.terraform.io/Kalpana_TFE/vnet/azurerm"
-  version = "2.6.0"
+module "network" {
+  source  = "app.terraform.io/Kalpana_TFE/network/azurerm"
+  version = "3.5.0"
   resource_group_name  = azurerm_resource_group.myresourcegroup.name
   # insert required variables here
 }
